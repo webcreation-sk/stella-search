76d7faf14bc087b01f40ba41a8d97bffb7994bda
Michal Babic <michal.babic@webcreation.sk>
2021-02-20 12:58:25 +0100

    - deleted multi index search - renamed api endpoint for search

--- src/Search/Document.php
+++ src/Search/Document.php
@@ -66,48 +66,7 @@ final class Document
             'include_to_statistics' => $includeToStatistics,
         ];
 
-        $res = $this->guzzleClient->get('/index/' . $this->index->getId(), $request);
-        return json_decode($res->getBody()->getContents(), true);
-    }
-
-    /**
-     * @param array $indices
-     * @param string $search
-     * @param int $page
-     * @param int $perPage
-     * @param array $filter
-     * @param string|null $sortBy
-     * @param string|null $sortDirection
-     * @param bool $includeToStatistics
-     * @return mixed
-     */
-    public function searchMultiIndex(
-        array $indices,
-        string $search = '',
-        int $page = 1,
-        int $perPage = 20,
-        array $filter = [],
-        string $sortBy = null,
-        string $sortDirection = null,
-        bool $includeToStatistics = true
-    )
-    {
-        if (isset($sortDirection) && !in_array(strtolower($sortDirection), SortDirections::DIRECTIONS)) {
-            throw new \InvalidArgumentException('Wrong sort direction');
-        }
-
-        $request['json'] = [
-            'indices' => $indices,
-            'q' => $search,
-            'page' => $page,
-            'per_page' => $perPage,
-            'filter' => $filter,
-            'sort_by' => $sortBy,
-            'sort_direction' => $sortDirection,
-            'include_to_statistics' => $includeToStatistics,
-        ];
-
-        $res = $this->guzzleClient->get('/index/' . $this->index->getId() . '/multi-index', $request);
+        $res = $this->guzzleClient->get('/index/' . $this->index->getId() . '/search', $request);
         return json_decode($res->getBody()->getContents(), true);
     }
 
